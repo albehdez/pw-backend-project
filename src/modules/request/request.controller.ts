2a08b59b7a8984b9104c093918f80a53568c5cd2
Java4602 <jp02042866@gmail.com>
2024-05-29 19:14:48 -0400

    Request Controller

--- src/modules/request/request.controller.ts
+++ src/modules/request/request.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { RequestService } from "./request.service";
+import { request } from "./entities";
+import { CreateProgramingDto } from "../programing/dto";
+import { CreateTransportDto } from "../transport/dto";
+import { CreateRequestDto, UpdateRequestDto } from "./dto";
 
-@Controller('request')
-export class RequestController {}
+@Controller("request")
+export class RequestController {
+  constructor(private readonly requestService: RequestService) {}
+
+  @Get()
+  get_requests(): Promise<request[]> {
+    return this.requestService.get_requests();
+  }
+
+  @Get(":id")
+  get_request(@Param("id") id: number): Promise<request> {
+    return this.requestService.get_request(id);
+  }
+
+  @Post()
+  create_request(
+    @Body() createrequestDto: CreateRequestDto,
+    createtransportDto: CreateTransportDto
+  ): Promise<request> {
+    return this.requestService.create_request(
+      createrequestDto,
+      createtransportDto
+    );
+  }
+
+  @Post(":id")
+  update_request(
+    @Param("id") id: number,
+    @Body() updateRequestDto: UpdateRequestDto
+  ) {
+    return this.requestService.update_request(id, updateRequestDto);
+  }
+
+  @Delete(":id")
+  delete_request(@Param("id") id: number): Promise<void> {
+    return this.requestService.delete_request(id);
+  }
+}
